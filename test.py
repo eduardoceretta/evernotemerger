15fd7a9ee9dfbb1e17e7707e53821fd89b240fed
Eduardo C Dalla Favera <eduardo.ceretta+github@gmail.com>
2020-09-14 00:19:55 +0200

    Note generation done

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import sys
 import os
 import json
 import re
+import traceback
 
 try:
     import ssl
@@ -30,7 +31,12 @@ import evernote.edam.notestore.NoteStore as NoteStore
 import thrift.protocol.TBinaryProtocol as TBinaryProtocol
 import thrift.transport.THttpClient as THttpClient
 # from socket import gaierror
+
+
 import html2text
+import markdown2
+from base64 import b64encode, b64decode
+from datetime import datetime
 
 
 DEBUG = True
@@ -42,8 +48,106 @@ def LOG(*args):
 
 VERSION = "0.1.0"
 
+def datestr(d):
+    d = datetime.fromtimestamp(d // 1000)
+    n = datetime.now()
+    delta = n - d
+    if delta.days == 0:
+        if delta.seconds <= 3600 == 0:
+            if delta.seconds <= 60 == 0:
+                return "just now"
+            else:
+                return "few minutes ago"
+        else:
+            return "few hours ago"
+    elif delta.days == 1:
+        return "yesterday"
+    elif delta.days == 2:
+        return "2 days ago"
+    return d.strftime("on %d/%m/%y")
+
+
+ecode = EDAMErrorCode
+error_groups = {
+        'server': ('Internal server error', [ecode.UNKNOWN, ecode.INTERNAL_ERROR, ecode.SHARD_UNAVAILABLE, ecode.UNSUPPORTED_OPERATION ]),
+        'data': ('User supplied data is invalid or conflicting', [ecode.BAD_DATA_FORMAT, ecode.DATA_REQUIRED, ecode.DATA_CONFLICT, ecode.LEN_TOO_SHORT, ecode.LEN_TOO_LONG, ecode.TOO_FEW, ecode.TOO_MANY]),
+        'permission': ('Action not allowed, permission denied or limits exceeded', [ecode.PERMISSION_DENIED, ecode.LIMIT_REACHED, ecode.QUOTA_REACHED, ecode.TAKEN_DOWN, ecode.RATE_LIMIT_REACHED]),
+        'auth': ('Authorisation error, consider re-configuring the plugin', [ecode.INVALID_AUTH, ecode.AUTH_EXPIRED]),
+        'contents': ('Illegal note contents', [ecode.ENML_VALIDATION])
+    }
+
+
+def errcode2name(err):
+    name = ecode._VALUES_TO_NAMES.get(err.errorCode, "UNKNOWN")
+    name = name.replace("_", " ").capitalize()
+    return name
+
+
+def err_reason(err):
+    for reason, group in error_groups.values():
+        if err.errorCode in group:
+            return reason
+    return "Unknown reason"
+
+def printError(msg):
+    print(msg)
+
+def explain_error(err):
+    if isinstance(err, EDAMUserException):
+        printError("Evernote error: [%s]\n\t%s" % (errcode2name(err), err.parameter))
+        if err.errorCode in error_groups["contents"][1]:
+            explanation = "The contents of the note are not valid.\n"
+            msg = err.parameter.split('"')
+            what = msg[0].strip().lower()
+            if what == "element type":
+                return explanation +\
+                    "The inline HTML tag '%s' is not allowed in Evernote notes." %\
+                    msg[1]
+            elif what == "attribute":
+                if msg[1] == "class":
+                    return explanation +\
+                        "The note contains a '%s' HTML tag "\
+                        "with a 'class' attribute; this is not allowed in a note.\n"\
+                        "Please use inline 'style' attributes or customise "\
+                        "the 'inline_css' setting." %\
+                        msg[3]
+                else:
+                    return explanation +\
+                        "The note contains a '%s' HTML tag"\
+                        " with a '%s' attribute; this is not allowed in a note." %\
+                        (msg[3], msg[1])
+            return explanation + err.parameter
+        else:
+            return err_reason(err)
+    elif isinstance(err, EDAMSystemException):
+        printError("Evernote error: [%s]\n\t%s" % (errcode2name(err), err.message))
+        return "Evernote cannot perform the requested action:\n" + err_reason(err)
+    elif isinstance(err, EDAMNotFoundException):
+        printError("Evernote error: [%s = %s]\n\tNot found" % (err.identifier, err.key))
+        return "Cannot find %s" % err.identifier.split('.', 1)[0]
+    elif isinstance(err, gaierror):
+        printError("Evernote error: [socket]\n\t%s" % str(err))
+        return 'The Evernote services seem unreachable.\n'\
+               'Please check your connection and retry.'
+    else:
+        printError("Evernote plugin error: %s" % str(err))
+        return 'Evernote plugin error, please see the console for more details.\nThen contact developer at\n'\
+               'https://github.com/bordaigorl/sublime-evernote/issues'
+
+
 class Evernote():
     USER_AGENT = {'User-Agent': 'EvernoteMerger:' + VERSION}
+
+    MD_EXTRAS = {
+        'footnotes'          : None,
+        'cuddled-lists'      : None,
+        'metadata'           : None,
+        'markdown-in-html'   : None,
+        'fenced-code-blocks' : {'noclasses': True, 'cssclass': "", 'style': "default"}
+    }
+
+    EVERNOTE_COMMENT_BEG = "<!-- EvernoteMerger:"
+    EVERNOTE_COMMENT_END = "-->"
 
     def __init__(self, token, noteStoreUrl):
         if not token :
@@ -62,6 +166,30 @@ class Evernote():
             self.noteStoreClient = NoteStore.Client(noteStoreProtocol)
         return self.noteStoreClient
 
+    def _convertContent(self, contents):
+        body = markdown2.markdown(contents, extras=Evernote.MD_EXTRAS)
+
+        wrapper_style = ''
+        if 'inline-css' in Evernote.MD_EXTRAS:
+            wrapper_style = Evernote.MD_EXTRAS['inline-css'].get('body', "")
+            if len(wrapper_style) > 0:
+                wrapper_style = ' style="%s"' % wrapper_style
+
+        meta = body.metadata or {}
+        content = '<?xml version="1.0" encoding="UTF-8"?>'
+        content += '<!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">'
+        content += '<en-note%s>' % wrapper_style
+        hidden = ('\n%s%s%s\n' %
+                    (Evernote.EVERNOTE_COMMENT_BEG,
+                     b64encode(contents.encode('utf8')).decode('utf8'),
+                     Evernote.EVERNOTE_COMMENT_END))
+        content += hidden
+        content += body
+        # LOG(body)
+        content += '</en-note>'
+        return content
+
+
     def getNotebooks(self):
         noteStore = self._getNoteStoreClient()
         return noteStore.listNotebooks(self.token)
@@ -73,7 +201,30 @@ class Evernote():
             notebook.name = name
             notebooks = noteStore.createNotebook(self.token, notebook)
         except Exception as e:
+            LOG(explain_error(e))
             LOG(e)
+            track = traceback.format_exc()
+            print(track)
+
+            return None
+
+    def createNote(self, notebook, title, tagNames, content, isMarkdown=False):
+        try:
+            noteStore = self._getNoteStoreClient()
+            note = Types.Note()
+            note.title = title
+            note.notebookGuid = notebook.guid
+            note.tagNames = tagNames
+            note.content = self._convertContent(content) if isMarkdown else content
+            cnote = noteStore.createNote(self.token, note)
+            return note
+
+        except Exception as e:
+            LOG(explain_error(e))
+            LOG(e)
+            track = traceback.format_exc()
+            print(track)
+
             return None
 
     def getNotes(self, notebook):
@@ -81,17 +232,17 @@ class Evernote():
             noteStore = self._getNoteStoreClient()
             return noteStore.findNotesMetadata(
                 self.token,
-                NoteStore.NoteFilter(
-                    # {
-                      notebookGuid=notebook.guid
-                    # }
-                ),
+                NoteStore.NoteFilter(notebookGuid=notebook.guid),
                 None,
                 1000,
                 NoteStore.NotesMetadataResultSpec(includeTitle=True, includeNotebookGuid=True, includeContentLength=True, includeCreated=True)
             ).notes
         except Exception as e:
+            LOG(explain_error(e))
             LOG(e)
+            track = traceback.format_exc()
+            print(track)
+
             return None
 
     def getNoteContent(self, note):
@@ -102,7 +253,10 @@ class Evernote():
                 note.guid,
             )
         except Exception as e:
+            LOG(explain_error(e))
             LOG(e)
+            track = traceback.format_exc()
+            print(track)
             return None
 
 
@@ -112,22 +266,43 @@ noteStoreUrl = os.environ['EVERNOTE_DEV_NOTESTORE_URL']
 
 ev = Evernote(token, noteStoreUrl)
 notebooks = ev.getNotebooks()
-print("notebooks")
-# print(['%s.%s (%s)' % (x.stack, x.name, x.guid) for x in notebooks])
-nb = next(filter(lambda x: x.stack is None and x.name == "Input", notebooks))
-print("notebook")
-print(nb)
-notes = ev.getNotes(nb)
-print("notes")
-print(['%s %s %s %s' % (x.title, x.guid, x.contentLength, x.created) for x in notes])
+
+# BACKUP ALL NOTES:
+# for notebook in notebooks:
+#     print (f"Notebook:{notebook.stack}.{notebook.name} - {notebook.guid}")
+#     notes = ev.getNotes(notebook)
+#     for note in notes:
+#         print (f"  Title:{note.title}")
+#         print (f"  GUID:{note.guid}")
+#         print (f"  Length:{note.contentLength}")
+#         content = ev.getNoteContent(note)
+#         mdtxt = html2text.html2text(content)
+#         print (mdtxt)
+#         print ("=============================")
+#     print ("++++++++++++++++++++++++++++++++++")
+
+
+notebook = next(filter(lambda x: x.stack is None and x.name == "Input", notebooks))
+notes = ev.getNotes(notebook)
+
+contents = ""
 for note in notes:
-    print (f"Title:{note.title}")
-    print (f"  GUID:{note.guid}")
-    print (f"  Length:{note.contentLength}")
     content = ev.getNoteContent(note)
     mdtxt = html2text.html2text(content)
-    print (mdtxt)
-    print ("=============================")
+    createdTime = datetime.fromtimestamp(note.created // 1000).strftime("%Y-%m-%d %H:%M:%S")
+    contents += f"""
+# {note.title}
 
+{mdtxt}
 
+_CreatedAt: {createdTime}_
+
+"""
+
+time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+contents += f"\n\n\n--------------------------\n_Merge generated at {time}_"
+
+# # print ("CONTENTS:\n",contents, "\nEND_CONTENTS")
+mergedNote = ev.createNote(notebook, "EvernoteMergerTest", [], contents, True)
+# print("NOTE", mergedNote)
 print("done")
